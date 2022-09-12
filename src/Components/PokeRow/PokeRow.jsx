a6fe40c4e6088237ecdbe791b9c3ca394aa5a736
Wesley Fernandes <wesleyintelligence@protonmail.com>
2022-09-12 12:57:50 -0300

    Reconfigurando home

--- src/Components/PokeRow/PokeRow.jsx
+++ src/Components/PokeRow/PokeRow.jsx
@@ -4,7 +4,7 @@ import Style from "./PokeRow.css"
 
 export default function PokeRow(props) {
 
-  const jumper = `http://127.0.0.1:5173/Pokemon/?pokemon=${props.url}`
+  const jumper = `http://wesleydev.ga/Pokemon/?pokemon=${props.url}`
   return (
     <a href={jumper}><Alert className='PokeRow' variant="dark">{props.name}</Alert></a>
   )
